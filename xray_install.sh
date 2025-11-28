ce05132a120577c646d2432f9a5bef73a4ad00cf
79544498 <135818764+79544498@users.noreply.github.com>
2025-11-28 21:52:34 +0800

    Add files via upload

---
+++ xray_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+apt update
+apt install -y ssh vim curl zip wget openssl
+
+cat >/etc/sysctl.d/fq.conf <<EOF
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+
+sysctl --system
+
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u root
+
+wget -O /root/config.bin https://raw.githubusercontent.com/79544498/79544498_script/main/config.bin
+
+openssl enc -d -aes-256-cbc -in /root/config.bin -out /usr/local/etc/xray/config.json
+
+systemctl enable xray
+systemctl restart xray
